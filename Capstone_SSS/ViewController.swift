3302ca1e065a476f8dca923a3e044870095372e7
KangMingyo <kang1@KangMingyoui-MacBookPro.local>
2022-05-02 18:47:44 +0900

    feat: 서버에 영상 업로드

--- Capstone_SSS/ViewController.swift
+++ Capstone_SSS/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var videoURL: URL?
+    var videoData: Data?
     var thumbnail: UIImage?
     
     var testImage: UIImage?
@@ -30,10 +31,10 @@ class ViewController: UIViewController {
 
     @IBAction func addImage(_ sender: UIBarButtonItem) {
         
-        imgPickerController.sourceType = .photoLibrary
-        self.present(imgPickerController, animated: true, completion: nil)
+//        imgPickerController.sourceType = .photoLibrary
+//        self.present(imgPickerController, animated: true, completion: nil)
 
-        //-VideoHelper.startMediaBrowser(delegate: self, sourceType: .savedPhotosAlbum)
+        VideoHelper.startMediaBrowser(delegate: self, sourceType: .savedPhotosAlbum)
     }
     
     //신고하기 버튼 클릭 되었을 때
@@ -41,7 +42,7 @@ class ViewController: UIViewController {
     //file:///private/var/mobile/Containers/Data/PluginKitPlugin/F72A5C2F-3B45-4C40-AAEB-7DAC95C46EC8/tmp/trim.587087F5-A1A7-449D-9B9C-4660E4EAAB55.MOV
     //341635 bytes
         do {
-            uploadImage(imageData: testImage)
+            uploadImage(imageURL: videoURL!)
             //let data = try Data(contentsOf: videoURL!, options: .mappedIfSafe)
             //-let data = thumbnail?.jpegData(compressionQuality: 0.8)
 //-        print(videoURL!)
@@ -53,65 +54,83 @@ class ViewController: UIViewController {
     
 }
 
-func uploadImage(imageData: UIImage?) {
+func uploadImage(imageURL: URL?) {
         
-        let URL = "http://127.0.0.1:8000/uploadfiles/"
+        let URL = "http://127.0.0.1:8000/uploadfiles"
 //    let URL = "192.168.0.55"
         let header : HTTPHeaders = [
             "Content-Type" : "application/json"]
+    
+//    let timestamp = NSDate().timeIntervalSince197
+    
+    AF.upload(multipartFormData: { multipartFormData in
+        multipartFormData.append(imageURL!, withName: "files", fileName: "video.mp4", mimeType: "video/mp4")
+        
+    }, to: URL, usingThreshold: UInt64.init(), method: .post, headers: header).response { response in
+        guard let statusCode = response.response?.statusCode,
+              statusCode == 200
+        else { return }
 
-        AF.upload(multipartFormData: { multipartFormData in
-            if let image = imageData?.jpegData(compressionQuality: 0.8) {
-                multipartFormData.append(image, withName: "files", fileName: "\(image).jpeg", mimeType: "image/jpeg")
-            }
-        }, to: URL, usingThreshold: UInt64.init(), method: .post, headers: header).response { response in
-            guard let statusCode = response.response?.statusCode,
-                  statusCode == 200
-            else { return }
+//        AF.upload(multipartFormData: { multipartFormData in
+//            if let image = imageData?.jpegData(compressionQuality: 0.8) {
+//                multipartFormData.append(image, withName: "files", fileName: "\(image).jpeg", mimeType: "image/jpeg")
+//            }
+//        }, to: URL, usingThreshold: UInt64.init(), method: .post, headers: header).response { response in
+//            guard let statusCode = response.response?.statusCode,
+//                  statusCode == 200
+//            else { return }
             //completion(.success(statusCode))
     }
 }
-//-    enum VideoHelper {
-//      static func startMediaBrowser(
-//        delegate: UIViewController & UINavigationControllerDelegate & UIImagePickerControllerDelegate,
-//        sourceType: UIImagePickerController.SourceType
-//      ) {
-//        guard UIImagePickerController.isSourceTypeAvailable(sourceType)
-//          else { return }
-//
-//        let mediaUI = UIImagePickerController()
-//        mediaUI.sourceType = sourceType
-//        mediaUI.mediaTypes = [kUTTypeMovie as String]
-//        mediaUI.allowsEditing = true
-//        mediaUI.delegate = delegate
-//        delegate.present(mediaUI, animated: true, completion: nil)
-//      }
-//    }
+    enum VideoHelper {
+      static func startMediaBrowser(
+        delegate: UIViewController & UINavigationControllerDelegate & UIImagePickerControllerDelegate,
+        sourceType: UIImagePickerController.SourceType
+      ) {
+        guard UIImagePickerController.isSourceTypeAvailable(sourceType)
+          else { return }
+
+        let mediaUI = UIImagePickerController()
+        mediaUI.sourceType = sourceType
+        mediaUI.mediaTypes = [kUTTypeMovie as String]
+        mediaUI.allowsEditing = true
+        mediaUI.delegate = delegate
+        delegate.present(mediaUI, animated: true, completion: nil)
+      }
+    }
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        //-picker.dismiss(animated: true, completion: nil)
+        picker.dismiss(animated: true, completion: nil)
         
-        if let img = info[UIImagePickerController.InfoKey.originalImage]{
-            imageView.image = img as? UIImage
-            testImage = img as! UIImage
-                }
-                
-                dismiss(animated: true, completion: nil)
+//        if let img = info[UIImagePickerController.InfoKey.originalImage]{
+//            imageView.image = img as? UIImage
+//            testImage = img as! UIImage
+//                }
+//
+//                dismiss(animated: true, completion: nil)
         
-//-        if let pickedVideo = info[UIImagePickerController.InfoKey.mediaURL] as? URL {
-//            videoURL = pickedVideo
-//            //Thumbnail
-//            let myAsset = AVAsset(url: pickedVideo)
-//            let imageGenerator = AVAssetImageGenerator(asset: myAsset)
-//            let time: CMTime = CMTime(value: 600, timescale: 600)
-//            guard let cgImage = try? imageGenerator.copyCGImage(at: time, actualTime: nil) else { fatalError() }
-//            let uiImage = UIImage(cgImage: cgImage)
-//            thumbnail = uiImage
-//            imageView.image = uiImage
+        if let pickedVideo = info[UIImagePickerController.InfoKey.mediaURL] as? URL {
+            videoURL = pickedVideo
+            //viedo URL -> Data
+//            do {
+//                videoData = try Data(contentsOf: pickedVideo, options: .mappedIfSafe)
+//                print(videoData!)
+//            } catch {
+//
+//            }
+            
+            //Thumbnail
+            let myAsset = AVAsset(url: pickedVideo)
+            let imageGenerator = AVAssetImageGenerator(asset: myAsset)
+            let time: CMTime = CMTime(value: 600, timescale: 600)
+            guard let cgImage = try? imageGenerator.copyCGImage(at: time, actualTime: nil) else { fatalError() }
+            let uiImage = UIImage(cgImage: cgImage)
+            thumbnail = uiImage
+            imageView.image = uiImage
             
             
-//        }
+        }
     }
 }
 
